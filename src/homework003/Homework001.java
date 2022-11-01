8bef8f8f3230c454c886bf709c8936e699c5ece3
melihcan öztürk <melihcannozturkk@gmail.com>
2022-11-01 21:58:28 +0300

    odev

--- src/homework003/Homework001.java
+++ src/homework003/Homework001.java
@@ -7,22 +7,25 @@ public class Homework001 {
 		// BULAN PROGRAM.
 		// int nums[] = {1 ,13,13,15,13,2,5,6,13,1}
 
-		int nums[] = {1, 13, 13, 13, 2, 5, 13, 1};
-
+		int nums[] = { 13, 13, 1, 13, 13, 2, 5, 13, 1, 13, 2, 2, 2, 2, 13, 13, 2, 2, 13, 2 };
 		int toplam = 0;
 
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] == 13) {
-				i++;
-				while (nums[i] == 13) {
-					i++;
-				}
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] != 13) {
+				toplam += nums[i];
 
-			} else
-				toplam = toplam + nums[i];
+			} else if (nums[i] == 13) {
+
+				if (nums[i] == nums[i + 1]) {
+					i--;
+				}
+				i++;
+			}
 
 		}
-		System.out.println(toplam);
+		if (nums[nums.length - 2] != 13 && nums[nums.length - 1] != 13) {
+			toplam += nums[nums.length - 1];
+			
+		}System.out.println(toplam);
 	}
-
 }
